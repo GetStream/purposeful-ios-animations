85affb5879618086aea1bb27c3e5d00003358e9f
Amos Gyamfi <amosgyamfi.fi@gmail.com>
2021-10-26 14:15:34 +0300

    Initial Commit

---
+++ VirtualEventSwiftUIKit/VirtualEventSwiftUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualEventSwiftUIKitApp.swift
+//  VirtualEventSwiftUIKit
+//
+//  Created by amos.gyamfi@getstream.io on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct VirtualEventSwiftUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
