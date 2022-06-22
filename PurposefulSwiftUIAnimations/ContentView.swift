0aa175bb5463cd36a906f03968f0ee56cca35b35
Amos Gyamfi <amosgyamfi.fi@gmail.com>
2022-06-22 13:11:45 +0300

    Initial Commit

---
+++ PurposefulSwiftUIAnimations/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  PurposefulSwiftUIAnimations
+//
+//  Created by amos.gyamfi@getstream.io on 22.6.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PurposefulSwiftUIAnimations/PurposefulSwiftUIAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurposefulSwiftUIAnimationsApp.swift
+//  PurposefulSwiftUIAnimations
+//
+//  Created by amos.gyamfi@getstream.io on 22.6.2022.
+//
+
+import SwiftUI
+
+@main
+struct PurposefulSwiftUIAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
